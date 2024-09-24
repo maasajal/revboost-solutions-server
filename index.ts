649be6e9aa9541f5b37b35fc80579aca62a1656b
rajiul93 <rajiulislam283@gmail.com>
2024-09-24 22:27:44 +0600

    jwt middleware dashboard cheker

--- index.ts
+++ index.ts
@@ -1,14 +1,17 @@
 import express, { Express, Request, Response } from "express";
 import authenticationRouter from "./routes/authentication.routes";
+import authorization from "./routes/authorization/authorization";
 import { ConnectDB } from "./routes/db.config";
 
 const app: Express = express();
-
 const PORT: number = 3000;
+
 ConnectDB();
+
 app.use(express.json());
 
 app.use("/api/v1", authenticationRouter); // এই লাইন মুলত আমাদের authentication  API গুলাকে হিট করবে
+app.use("/authorize", authorization) // এই রাউট অথরাইজেশন এর জন্যে কাজ করবে এবং সে authorization.ts রাউটে যাবে
 
 app.get("/", (req: Request, res: Response) => {
   res.send({ message: "Welcome to the RevBoost Solutions server!" });
--- routes/authentication.routes.ts
+++ routes/authentication.routes.ts
@@ -1,24 +1,38 @@
 import express, { Request, Response, Router } from "express";
+import { generateToken } from "../jwt/jwt";
 import { RegisterModel } from "../models/register.models";
 const router: Router = express.Router();
 
 router.route("/register").post(async (req: Request, res: Response) => {
   try {
     const { name, photo, email, password } = req.body;
-    const result = await RegisterModel.create({ name, photo, email, password });
-    console.log(result)
+    const data = req.body;
+    const existingUser = await RegisterModel.findOne({ email });
+    // check user already exist or not
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: `this email:${email} already exist` });
+    }
+
+    const result = await RegisterModel.create(data);
     res.status(201).send(result);
   } catch (error) {
-    console.error("Error saving data:", error);
-    res.status(500).send("Error saving data");
+    res.status(500).send("Error saving data t");
   }
 });
 
-router.route("/register").get(async (req: Request, res: Response) => {
-  try { 
-    const result = await RegisterModel.find({});
-    console.log(result)
-    res.status(200).send(result);
+router.route("/login").post(async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body; 
+    const result = await RegisterModel.findOne({ email }); 
+    if (result?.email == email && result?.password === password) {
+      //check user validation
+      const token = generateToken(email); // create token
+      res.status(200).send({ message: token }); // send token
+    } else {
+      res.status(403).json({ message: "is not valid user Forbidden  access " });
+    }
   } catch (error) {
     console.error("Error saving data:", error);
     res.status(500).send("Error saving data");
