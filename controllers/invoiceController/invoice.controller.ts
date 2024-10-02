f5af5fc68e6ed55c73d42d830b4c782a73bedcbe
Bilshansakib <burakaghani@gmail.com>
2024-10-03 03:56:08 +0600

    Update: invoiceController/ Get All Invoices

--- controllers/invoiceController/invoice.controller.ts
+++ controllers/invoiceController/invoice.controller.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from "express";
 import { InvoiceModel } from "../../models/companyIncomes/Invoice.model";
-export const createInvoices = async (req: Request, res: Response)=>{
-    try {
-        const newInvoice = new InvoiceModel(req.body);
-        await newInvoice.save();
-        res.status(201).send(newInvoice);
-      } catch (error) {
-        res.status(400).send(error);
-      }
-}
+export const createInvoices = async (req: Request, res: Response) => {
+  try {
+    const newInvoice = new InvoiceModel(req.body);
+    await newInvoice.save();
+    res.status(201).send(newInvoice);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const allInvoices = async (req: Request, res: Response) => {
+  try {
+    const invoices = await InvoiceModel.find();
+    res.send(invoices);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
