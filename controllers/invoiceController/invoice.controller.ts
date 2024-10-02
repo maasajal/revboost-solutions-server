cbe6555dca4f331a6183055de666c0da8e854768
Bilshansakib <burakaghani@gmail.com>
2024-10-03 03:45:39 +0600

    Update: invoiceController/created invoices controller

---
+++ controllers/invoiceController/invoice.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import { InvoiceModel } from "../../models/companyIncomes/Invoice.model";
+export const createInvoices = async (req: Request, res: Response)=>{
+    try {
+        const newInvoice = new InvoiceModel(req.body);
+        await newInvoice.save();
+        res.status(201).send(newInvoice);
+      } catch (error) {
+        res.status(400).send(error);
+      }
+}
