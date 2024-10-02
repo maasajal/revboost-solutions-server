be57a500d337befca4cced85df4a778ae06ec137
Bilshansakib <burakaghani@gmail.com>
2024-10-03 04:00:25 +0600

    Update: invoice router for Get All Invoice

--- routes/invoiceRoutes/invoice.routes.ts
+++ routes/invoiceRoutes/invoice.routes.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { createInvoices } from "../../controllers/invoiceController/invoice.controller";
+import { allInvoices, createInvoices } from "../../controllers/invoiceController/invoice.controller";
 const router = express.Router();
 
 router.post("/api/invoices", createInvoices)
+router.get("/api/invoices", allInvoices)
 
 export default router
