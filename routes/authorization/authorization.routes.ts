ef3b9c7122b37c307f3f2e0ea8ec361248608b01
rajiul93 <rajiulislam283@gmail.com>
2024-10-09 20:22:17 +0600

    create basic, standard and premium plan for middleware also admin roout for api

--- routes/authorization/authorization.routes.ts
+++ routes/authorization/authorization.routes.ts
@@ -1,5 +1,10 @@
 import express, { Router } from "express";
-import { basicPlan, isAdmin, premiumPlan, standardPlan } from "../../controllers/authorizationController/authorization.controller";
+import {
+    basicPlan,
+    isAdmin,
+    premiumPlan,
+    standardPlan,
+} from "../../controllers/authorizationController/authorization.controller";
 import {
     adminMiddleware,
     authMiddleware,
@@ -11,8 +16,11 @@ const router: Router = express.Router();
 
 router.route("/admin").get(authMiddleware, adminMiddleware, isAdmin);
 router.route("/basic").get(authMiddleware, basicPlanMiddleware, basicPlan);
-router.route("/standard").get(authMiddleware, standardPlanMiddleware, standardPlan);
-router.route("/premium").get(authMiddleware, premiumPlanMiddleware, premiumPlan);
-
+router
+  .route("/standard")
+  .get(authMiddleware, standardPlanMiddleware, standardPlan);
+router
+  .route("/premium")
+  .get(authMiddleware, premiumPlanMiddleware, premiumPlan);
 
 export default router;
