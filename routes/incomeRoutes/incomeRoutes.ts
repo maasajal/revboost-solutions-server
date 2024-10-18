912155d6b7502ad2a125fb117e1f6abe5c2b3e7a
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-10-18 12:53:44 +0300

    Income get and post successfully implement

--- routes/incomeRoutes/incomeRoutes.ts
+++ routes/incomeRoutes/incomeRoutes.ts
@@ -3,14 +3,12 @@ import {
   addOrUpdateIncome,
   deleteIncome,
   getIncomes,
-  updateIncomeEntries,
 } from "../../controllers/incomeController/incomeController";
 
 const router = express.Router();
 
 router.get("/:userId", getIncomes);
 router.post("/add-update-income", addOrUpdateIncome);
-router.patch("/add-income/:userId", updateIncomeEntries);
 router.delete("/:userId/:incomeId", deleteIncome);
 
 export default router;
